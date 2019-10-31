74add55d9d0cefd5f52c1f9bb09589f5b8b4c234
roland <roland@catalogix.se>
2019-10-31 08:56:43 +0100

    Changed oic version dependency.

--- setup.py
+++ setup.py
@@ -46,7 +46,7 @@ setup(
         "requests >= 2.0.0",
         'pyYAML',
         'pycryptodomex',
-        'oic >= 1.0.0',
+        'oic >= 1.1.0',
         'pyjwkest >= 1.4.2',
         'future',
         'entropy',
